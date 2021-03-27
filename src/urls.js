87601c5366d446797269e6eb54eed00a39cfc933
EDEL <edelellenmurray@hotmail.com>
2021-03-27 14:26:17 +0000

    added async func, load balancing, config urls

---
+++ src/urls.js
@@ -0,0 +1,10 @@
+var url_paths = {
+    add  : "add/",
+    subtract  : "subtract/",
+    multiply  : "multiply/",
+    divide  : "divide/",
+    exponent  : "exp/",
+    modulus  : "modulus/",  
+  };
+var proxy_list = ['http://proxy.40146762.qpc.hal.davecutting.uk/',
+                'http://webcalc.40146762.qpc.hal.davecutting.uk/']
