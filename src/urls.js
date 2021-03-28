33e2f7ca26c158ba972e0c511e2dad34c45b7436
EDEL <edelellenmurray@hotmail.com>
2021-03-28 16:02:30 +0100

    update

--- src/urls.js
+++ src/urls.js
@@ -6,5 +6,9 @@ var url_paths = {
     exponent  : "exponent/",
     modulus  : "modulus/",  
   };
+  //could have formatted with just changing the subdomain as end always same
 var proxy_list = ['http://proxy.40146762.qpc.hal.davecutting.uk/',
-                'http://webcalc.40146762.qpc.hal.davecutting.uk/']
+                  'http://proxy1.40146762.qpc.hal.davecutting.uk/',
+                  'http://proxy2.40146762.qpc.hal.davecutting.uk/',
+                  'http://proxy3.40146762.qpc.hal.davecutting.uk/',
+                  'http://webcalc.40146762.qpc.hal.davecutting.uk/']
